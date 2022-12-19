9534ec9492571c8b3f4053d78eff5888f56c5c5c
hilemnurerdem <hilemerdm7@gmail.com>
2022-12-19 15:56:42 +0300

    create-project and add-colors,login screen

---
+++ WeatherAPP/WeatherAPP/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  WeatherAPP
+//
+//  Created by Hilem nur Erdem on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    @IBAction func didTapLogIn(_ sender: Any) {
+        print("user e-mail is -> \(emailTextField.text ?? "not data for e-mail")\nuser password is -> \(passwordTextField.text ?? "not data for password")")
+        
+    }
+    
+
+
+}
+
